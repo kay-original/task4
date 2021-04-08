d59733b76b70064d879a650e4be61b87d97805b6
kay-orijinal <kay.0122.27.soccer@icloud.com>
2021-04-08 20:48:10 +0900

    Update task4.js

--- task4.js
+++ task4.js
@@ -1,8 +1,23 @@
-let display=document.getElementById("display");
+const display = document.getElementById("display");
+const plus = document.getElementById("plus");
+const divided = document.getElementById("divided");
+const multi = document.getElementById("multi");
+const minus = document.getElementById("minus");
 
+function initial(){
+  plus.disabled = true;
+  divided.disabled = true;
+  multi.disabled = true;
+  minus.disabled = true;
+}
 function edit(elem){
   display.value = display.value+elem.value;
+  plus.disabled = false;
+  divided.disabled = false;
+  multi.disabled = false;
+  minus.disabled = false;
 }
 function calc(){
   display.value = new Function("return " + display.value)();
 }
+initial();
